15cfda278bf05b0941a7d2c828b8b72496f040cd
CrazyVibes07 <crazyvibes07@pop-os.localdomain>
2021-06-17 20:25:14 +0100

    Please don't ask what I'm doing.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Tall_and_Male
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.Write("Are you a male? (True or False): ");
+            bool isMale = Convert.ToBoolean(Console.ReadLine());
+            Console.Write("Are you tall? (True or False): ");
+            bool isTall = Convert.ToBoolean(Console.ReadLine());
+
+            if (isMale && isTall)
+            {
+                Console.WriteLine("You are a tall male.");
+            } else if (isMale && !isTall){
+                Console.WriteLine("You are a short male.");
+            } else if (!isMale && isTall){
+                Console.WriteLine("You are not a male but you are tall.");
+            } else
+            {
+                Console.WriteLine("You are not a male and not tall.");
+            }
+        }
+    }
+}
